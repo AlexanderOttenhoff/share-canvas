8896936032f8e0aa3b58c6011163e54127ba0243
gsuess <garic.suess@gmail.com>
2015-10-10 18:31:09 +0200

    Current Game

--- both/models/games.js
+++ both/models/games.js
@@ -24,4 +24,10 @@ var sampleSchema = {
 	roomId: "ID", // Not used at the moment, but possibly useful in the future
 	drawer: "userID", // ID of player with draw priviledges
 	solution: "Bridge over the River Kwai" // Solution string of the game
-}
+};
+
+Games.current = (options = {}) => {
+	Games.findOne({endTime: {$exists: false}}, _.extend({
+		sort: {startTime: -1}
+	}, options));
+};
