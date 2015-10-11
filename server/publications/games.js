5d91edab72dd2906dde34a6325c2726b1fa3aab0
gsuess <garic.suess@gmail.com>
2015-10-11 11:18:34 +0200

    No drawer when there is no current game

--- server/publications/games.js
+++ server/publications/games.js
@@ -1,6 +1,6 @@
 Meteor.publish('games', function() {
 
-    var isDrawer = this.userId === Games.current().drawerId,
+    var isDrawer = Games.current() && this.userId === Games.current().drawerId,
       fields = isDrawer ? {} : {solution: 0};
 
     return Games.find({}, {
